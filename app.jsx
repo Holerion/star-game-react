ec2fe0bb8481b622bd3f606c297308b29c55706b
Holerion <qwere111133334444@gmail.com>
2023-02-08 14:50:05 +0200

    Update app.jsx

--- app.jsx
+++ app.jsx
@@ -3,7 +3,7 @@
 const StarsDisplay = props => (
     <>
       {utils.range(1, props.count).map(starId => (
-        <div key={starId} className="star" />
+        <div key={starId} className="star" />        
       ))}
     </>
   );
